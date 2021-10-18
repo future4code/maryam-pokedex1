0dd8c9866543837c3f354e1786f4eb5e71187241
Stephanie Flavio <stephdpmello@gmail.com>
2021-10-18 20:24:01 -0300

    comecei a criar o card das paginas e limpei o react

---
+++ pokedex/src/components/ContainerCards/Cards.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+
+export const Cards = (props) => {
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        component="img"
+        height="140"
+        image="/static/images/cards/contemplative-reptile.jpg"
+        alt="green iguana"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          {props.title}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          {props.text}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">{props.fistButton}</Button>
+        <Button size="small">{props.secondButton}</Button>
+      </CardActions>
+    </Card>
+  );
+}
