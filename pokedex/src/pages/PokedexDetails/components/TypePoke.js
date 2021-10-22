e6b65a4e04bb38f13976a092e76234c7b830b76e
Laura Laurena <lauralaurena0204@gmail.com>
2021-10-22 14:21:59 -0300

    Cards provisórios dos detalhes

--- pokedex/src/pages/PokedexDetails/components/TypePoke.js
+++ pokedex/src/pages/PokedexDetails/components/TypePoke.js
@@ -1,9 +1,41 @@
 import React from "react";
+import styled from "styled-components"
 
-export const TypePoke = () => {
+const ContainerTypes=styled.div`
+display: grid;
+box-shadow: 3px 3px 3px 3px lightgray;
+margin: 20px;
+padding:10px;
+justify-items: center;
+align-items: center;
+p{
+    margin: 10px;
+}
+h3{
+    margin: 10px;
+}
+`
+
+export const TypePoke = (props) => {
     return (
-        <div>
-            Type Poke
-        </div>
+    <div>
+        <ContainerTypes>
+            <h3>Type Poke</h3>
+            {/* <strong>{props.typeName}</strong> */}
+            <div>
+                <p>tipo 1</p>
+                <p>tipo 2</p>
+            </div>
+        </ ContainerTypes>
+        <ContainerTypes>
+            <h3>Attack Types</h3>
+            {/* <strong>{props.attackName}</strong> */}
+            <p>attack1</p>
+            <p>attack2</p>
+            <p>attack3</p>
+            <p>attack4</p>
+        </ContainerTypes>
+        
+    </div>
     )
 }
