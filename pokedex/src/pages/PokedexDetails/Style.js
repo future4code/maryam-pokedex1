b0b8edcb812052a7369c8a7fd3318b5406a50f24
Stephanie Flavio <stephdpmello@gmail.com>
2021-10-22 09:31:48 -0300

    Feito o card do status do pokemon em detalhes

--- pokedex/src/pages/PokedexDetails/Style.js
+++ pokedex/src/pages/PokedexDetails/Style.js
@@ -2,5 +2,7 @@ import styled from "styled-components";
 
 export const Style = styled.div`
     display: grid;
+    grid-auto-flow: column;
     grid-template-columns: 1fr 1fr 1fr;
+    gap: 32px;
 `
